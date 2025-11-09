faba214d4cc408c0b017aa0146cc63145b3a4b1c
masterkreb <170341487+masterkreb@users.noreply.github.com>
2025-11-09 13:49:02 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,3 @@
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// import { defineConfig, loadEnv } from 'vite';
-// import react from '@vitejs/plugin-react';
-//
-// // FIX: In an ES module, __dirname is not available by default.
-// // This defines it using helpers from NodeJS's `url` and `path` modules.
-// const __dirname = path.dirname(fileURLToPath(import.meta.url));
-//
-// export default defineConfig(({ mode }) => {
-//     const env = loadEnv(mode, '.', '');
-//     return {
-//         server: {
-//             port: 3000,
-//             host: '0.0.0.0',
-//         },
-//         plugins: [react()],
-//         define: {
-//             'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-//             'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-//         },
-//         resolve: {
-//             alias: {
-//                 '@': path.resolve(__dirname, '.'),
-//             }
-//         },
-//         build: {
-//             rollupOptions: {
-//                 input: {
-//                     main: path.resolve(__dirname, 'index.html'),
-//                     admin: path.resolve(__dirname, 'admin.html'),
-//                 },
-//             },
-//         },
-//     };
-// });
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
