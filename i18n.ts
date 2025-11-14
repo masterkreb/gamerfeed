112f15d7b93fe2359169920199326097874cbb67
masterkreb <170341487+masterkreb@users.noreply.github.com>
2025-11-14 13:37:55 +0100

    added automatic Language Detector

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
     en: {
@@ -267,13 +268,17 @@ const resources = {
 };
 
 i18n
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources,
-        lng: 'de', // Standard: Deutsch
-        fallbackLng: 'en',
+        fallbackLng: 'de',
         interpolation: {
             escapeValue: false
+        },
+        detection: {
+            order: ['localStorage', 'navigator'],
+            caches: ['localStorage'],
         }
     });
 
